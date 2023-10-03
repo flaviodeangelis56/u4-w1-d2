4f31aca071725bab43e7fce456da39226146f0d0
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-10-03 14:49:18 +0200

    complete-Esercizio3

--- Esercizio3.java
+++ Esercizio3.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 
 public class Esercizio3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Inserisci una stringa da suddividere in caratteri, :q per interrompere il programma: ");
+          String input = scanner.nextLine();
+
+            if (input.equals(":q")) {
+                break;
+            }
+
+            String[] arrayCaratteri = input.split("");
+            String stringaFinale = "";
+
+            for (int i = 0; i < arrayCaratteri.length; i++) {
+                stringaFinale += arrayCaratteri[i];
+                if (i < arrayCaratteri.length - 1) {
+                    stringaFinale += ",";
+                }
+            }
+
+            System.out.println(stringaFinale);
+        }
+
+        scanner.close();
     }
 }
